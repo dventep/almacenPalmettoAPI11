b9da1caebb247204172e36b12b3dac6e03e8a6fc
David Venté <davidventepolo@gmail.com>
2023-03-09 14:55:10 -0500

    9-Mar 02 ~ se añade contraseña 'contrasena123' a mysql

--- microUsuarios/src/models/usuariosModel.js
+++ microUsuarios/src/models/usuariosModel.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2/promise');
 const connection = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: 'contrasena123',
     database: 'almacen'
 });
 async function traerUsuarios() {
